1cd04d59c2cc8909075229ab78cb0bcb4afcefa7
Marcello Krahforst <marcello.2001@hotmail.com>
2024-05-25 00:19:16 +0200

    add lib

--- hardware/steam-deck/default.nix
+++ hardware/steam-deck/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   # Fetch the "development" branch of the Jovian-NixOS repository
